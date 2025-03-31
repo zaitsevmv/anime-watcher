cd114a783d188e7859ed3c312a0022bdddb74f89
Matvey <qazsewpp@gmail.com>
2025-03-31 17:26:48 +0300

    search db logic

--- backend/database_logic/search_db/search_db.cpp
+++ backend/database_logic/search_db/search_db.cpp
@@ -1,17 +1,96 @@
 #include "search_db.hpp"
 
-std::optional<int32_t> SearchDB::LoadData() {
-    
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+AnimeSearchDB::~AnimeSearchDB() {
+    curl_global_cleanup();
 }
 
-std::optional<int32_t> SearchDB::AddDocument() {
-    
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) {
+    size_t newLength = size * nmemb;
+    try {
+        s->append((char*)contents, newLength);
+    } catch (std::bad_alloc& e) {
+        return 0;
+    }
+    return newLength;
 }
 
-std::optional<int32_t> SearchDB::DeleteDocument() {
-    
+void AnimeSearchDB::Start() {
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl.reset(curl_easy_init());
 }
 
-std::optional<std::string> SearchDB::ProcessSearchRequest(const std::string& search_request) {
-    
+std::optional<int32_t> AnimeSearchDB::AddAnime(const std::string& anime_hash, const std::string& anime_data_json) {
+    std::string url = "http://localhost:9200/" + db_name + "/_doc/" + anime_hash;
+    curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, WriteCallback);
+    std::string curl_buffer;
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &curl_buffer);
+
+    struct curl_slist* headers = NULL;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+
+    curl_easy_setopt(curl.get(), CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl.get(), CURLOPT_POSTFIELDS, anime_data_json.c_str());
+
+    auto result = curl_easy_perform(curl.get());
+    curl_easy_cleanup(curl.get());
+    if(result != CURLE_OK){
+        return std::nullopt;
+    }
+    return 1;
+}
+
+std::optional<int32_t> AnimeSearchDB::DeleteAnime(const std::string& anime_hash) {
+    std::string url = "http://localhost:9200/" + db_name + "/_doc/" + anime_hash;
+    curl_easy_setopt(curl.get(), CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, WriteCallback);
+    std::string curl_buffer;
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &curl_buffer);
+
+    auto result = curl_easy_perform(curl.get());
+    curl_easy_cleanup(curl.get());
+    if(result != CURLE_OK) {
+        return std::nullopt;
+    }
+    return 1;
+}
+
+std::optional<std::string> AnimeSearchDB::SearchAnime(const std::string& search_request) {
+    std::string url = "http://localhost:9200/" + db_name + "/_search?q=" + anime_name_field + ":" + search_request;
+    curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, WriteCallback);
+    std::string curl_buffer;
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &curl_buffer);
+
+    auto result = curl_easy_perform(curl.get());
+    curl_easy_cleanup(curl.get());
+    if(result != CURLE_OK) {
+        return std::nullopt;
+    }
+    return curl_buffer;
+}
+
+std::optional<int32_t> AnimeSearchDB::UpdateAnime(const std::string& anime_hash, const std::string& anime_data_json) {
+    std::string url = "http://localhost:9200/" + db_name + "/_update/" + anime_hash;
+    curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, WriteCallback);
+    std::string curl_buffer;
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &curl_buffer);
+
+    struct curl_slist* headers = NULL;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+
+    curl_easy_setopt(curl.get(), CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl.get(), CURLOPT_POSTFIELDS, anime_data_json.c_str());
+
+    auto result = curl_easy_perform(curl.get());
+    curl_easy_cleanup(curl.get());
+    if(result != CURLE_OK){
+        return std::nullopt;
+    }
+    return 1;
 }
--- backend/database_logic/search_db/search_db.hpp
+++ backend/database_logic/search_db/search_db.hpp
@@ -1,20 +1,31 @@
 #ifndef __SEARCH_DB__
 #define __SEARCH_DB__
 
+#include <curl/curl.h>
+
 #include <cstdint>
+#include <memory>
 #include <optional>
 #include <string>
 
-class SearchDB{
+class AnimeSearchDB{
 public:
-    std::optional<int32_t> LoadData();
+    ~AnimeSearchDB();
 
-    std::optional<int32_t> AddDocument();
-    std::optional<int32_t> DeleteDocument();
+    void Start();
 
-    std::optional<std::string> ProcessSearchRequest(const std::string& search_request);
+    std::optional<int32_t> AddAnime(const std::string& anime_hash, const std::string& anime_data_json);
+
+    std::optional<int32_t> DeleteAnime(const std::string& anime_hash);
+
+    std::optional<std::string> SearchAnime(const std::string& search_request);
+
+    std::optional<int32_t> UpdateAnime(const std::string& anime_hash, const std::string& anime_data_json);
 private:
-    
+    std::unique_ptr<CURL> curl;
+
+    std::string db_name = "anime_names";
+    std::string anime_name_field = "anime_name";
 };
 
 #endif
